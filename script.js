31df9dbb313ac51380daaeafd5c785eee7761273
Xusniddin <narkulof@gmail.com>
2022-04-02 11:44:22 +0500

    aksdjk

--- script.js
+++ script.js
@@ -6,14 +6,17 @@ const input = document.querySelector('input');
 const search = document.querySelector('label');
 ///////////////////////////////////////
 
-const getCountry = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(request.responseText);
+// const getCountry = function (country) {
+const request = new XMLHttpRequest();
+request.open('GET', `https://restcountries.com/v2/all`);
+request.send();
 
-    // data.forEach(data => {
+request.addEventListener('load', function () {
+  const data = JSON.parse(request.responseText);
+  console.log(data);
+
+  data.forEach(function (data) {
+    console.log(data);
     let html = `
 <article class="country">
   <img class="country__img" src="${data.flag}" />
@@ -27,15 +30,21 @@ const getCountry = function (country) {
       data.languages ? data.languages[0].nativeName : data.language
     }</p>
     <p class="country__row"><span>💰</span> ${
-      data.currencies ? data.currencies[0].name : data.currency
+      data.currencies
+        ? ` ${data.currencies[0].name} ${data.currencies[0].symbol}`
+        : data.currency
     }</p>
   </div>
 </article>
   `;
-    countriesContainer.innerHTML = html;
-    countriesContainer.style.opacity = 1;
+    search.addEventListener('click', function () {
+      if (input.value == data.name) {
+        countriesContainer.innerHTML += html;
+        countriesContainer.style.opacity = 1;
+      }
+    });
   });
-  // });
-};
+});
+// };
 
-getCountry('usa');
+// getCountry('usa');
